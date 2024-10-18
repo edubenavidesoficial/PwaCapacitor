bb0568466411c5b7687655c82fa61f15a1716715
Eduardo Benavides <edu3196.eb@gmail.com>
2024-10-18 14:00:27 -0500

    Cambios

--- my-pwa-app/my-pwa-app/src/main.js
+++ my-pwa-app/my-pwa-app/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { Browser } from '@capacitor/browser';
+import { SplashScreen } from '@capacitor/splash-screen';
 
-createApp(App).mount('#app')
+const openWebView = async () => {
+  await Browser.open({ url: 'https://mercadomi.com.ec/' });
+};
+
+const app = createApp(App);
+app.mount('#app');
+
+// Cuando el DOM esté listo y el splash haya terminado
+document.addEventListener('DOMContentLoaded', async () => {
+  await SplashScreen.hide();  // Cierra el splash screen
+  openWebView();              // Abre el WebView
+});
